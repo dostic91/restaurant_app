87be645f0a225706ed028bf7e11141a4f4469ac6
dostic91 <dosticdejan@gmail.com>
2022-12-22 02:17:51 +0100

    first_commit

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
+
+const Header = () => (
+  <div className='app__header app__wrapper section__padding' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading  title='Chase the new flavour'/>
+        <h1 className='app__header-h1'>The Key To Fine Dinning</h1>
+        <p className='p__opensans' style={{margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button className='custom__button'>Explore Menu</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img  src={images.welcome}/>
+      </div>
+  </div>
+);
+
+export default Header;
